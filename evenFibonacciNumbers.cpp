b4a408778ecfbf013a5133537644a26e01b6bd6c
IQBAL MAHAMUD <iqbalmahamud@IQBALs-MacBook-Pro.local>
2023-05-11 01:16:06 +0600

    even fibo sum

---
+++ evenFibonacciNumbers.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#define int long long int
+
+using namespace std;
+
+int dp(int n, map<int,int> &m){
+    if(n==1 || n==0) return n;
+    if(m[n]) return m[n]; 
+    m[n] = dp(n-1,m)+dp(n-2,m);
+    return m[n]; 
+}
+
+int32_t main(){
+    int t;  cin>>t;
+    map<int,int>m; 
+    dp(81,m);
+    while (t--)
+    {
+        int n,sum=0;  cin>>n;
+        for(auto [key,value]:m){
+            if(value>n) break;
+            if(value%2)  continue;
+            sum+=value;
+        }
+        cout<< sum << endl; 
+    }
+}
